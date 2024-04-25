6603b8556e0ee574c4c515ac24ab13d34129d98d
Arpit <arpit.mishra.out@gmail.com>
2024-04-25 08:25:14 +0530

    closures and lexical scoping in JavaScript

---
+++ closures.js
@@ -0,0 +1,26 @@
+`use strict`;
+
+// lexical scoping and closures
+// function init() {
+//   let name = "Mozilla"; // name is a local variable created by init
+//   function displayName() {
+//     // displayName() is the inner function, that forms the closure
+//     console.log(name); // use variable declared in the parent function
+//   }
+//   displayName();
+// }
+// init();
+
+// closures
+
+// function makeFunc() {
+//   const name = `Mozilla`;
+//   function displayName() {
+//     console.log(name);
+//   }
+
+//   return displayName;
+// }
+
+// const myFunc = makeFunc();
+// myFunc();
--- getter_setter.js
+++ getter_setter.js
@@ -11,9 +11,26 @@ class User {
   }
 
   set password(value) {
-    return (this._password = value);
+    this._password = value;
   }
 }
 
 const user1 = new User(`hitesh123.ai`, `12345678`);
-console.log(user1.password);
+// console.log(user1.password);
+
+// now setting getter and setter for the object
+
+const user = {
+  _email: `ap@gmail.com`,
+  _password: `abc`,
+
+  get email() {
+    return this._email.toUpperCase();
+  },
+  set email(value) {
+    this._email = value;
+  },
+};
+
+let user2 = Object.create(user);
+console.log(user2.email);
